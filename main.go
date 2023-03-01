11bd79b4f27dab60f83c6b98d762b2941ad34f64
eduardor2m <deveduardomelo@gmail.com>
2023-03-01 00:51:46 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Elon Musk"
+	var numeroAgencia int = 123
+	var numeroConta int = 123456
+	var saldo float64 = 1000.50
+
+	fmt.Println("Titular: ", titular)
+	fmt.Println("Agencia: ", numeroAgencia)
+	fmt.Println("Conta: ", numeroConta)
+	fmt.Println("Saldo: ", saldo)
+
+}
